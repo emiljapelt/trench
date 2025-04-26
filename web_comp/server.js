9be0a0890cec782984b631ee142df0b11e446fcb
emiljapelt <ejapelt@hotmail.com>
2025-04-26 16:52:28 +0200

    Update server

--- web_comp/server.js
+++ web_comp/server.js
@@ -8,36 +8,52 @@ const app = express();
 const upload = multer();
 
 const form = `
-    <form action="filecheck" method="post" enctype="multipart/form-data">
-        <input type="file" name="file"/><br>
-        <input type="submit" value="Check"/>
-    </form>
+    <head>
+        <style>
+            div { margin: 0 0 10 10; }
+        </style>
+    </head>
+    <body>
+        <form action="filesubmit" method="post" enctype="multipart/form-data">
+            <div>
+                <label for="name">Name</label>
+                <input type="text" id="name" name="name"/><br>
+            </div>
+            <div>
+                <label for="file">File</label>
+                <input type="file" id="file" name="file"/><br>
+            </div>
+            <div>
+                <input type="submit" value="Submit"/>
+            </div>
+        </form>
+    </body>
 `;
 
 let counter = 0;
 
-app.get('/', (req, res) => {
+app.get(/.*/, (req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(form);
     res.send();
 });
 
-app.post('/filecheck', upload.single('file'), (req, res) => {
+app.post('/filesubmit', upload.single('file'), (req, res) => {
     const { body, file } = req;
-    if (file) {
+    if (file && body.name) {
         const content = Buffer.from(file.buffer).toString("utf-8");
-        const path = `./file_${counter}.tr`;
+        const path = `./file_${counter++}.tr`;
+        console.log(`${body.name} submitted ${path}`);
         fs.writeFileSync(path, content);
         exec(`../trenchc ${path}`, { cwd: '.'}, (err,stdout,stderr) => {
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.write(`${form}<p>${stdout}</p>`);
             res.send();
-            fs.unlink(path, (err) => {});
         });
     }
     else {   
         res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(form);
+        res.write(`${form}<p>Please select a file AND write a name >:(</p>`);
         res.send();
     }
 })
