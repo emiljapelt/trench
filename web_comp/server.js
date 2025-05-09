84e8dd43a60e6982ab1e3d3575f6c199f9bd79ba
emiljapelt <ejapelt@hotmail.com>
2025-05-09 16:30:42 +0200

    Add a few new comp server modes

--- web_comp/server.js
+++ web_comp/server.js
@@ -1,11 +1,19 @@
 // https://khendrikse.netlify.app/blog/send-data-with-formdata/#with-a-file-upload
 const express = require('express');
 const multer = require('multer');
+const cookie_parser = require('cookie-parser')
 const fs = require('node:fs');
 const { exec } = require('child_process');
 
 const app = express();
 const upload = multer();
+app.use(cookie_parser());
+
+const setting = process.argv[2];
+const secret = process.argv[3];
+let counter = 0;
+let map = {};
+
 
 const form = `
     <head>
@@ -54,15 +62,29 @@ const form = `
     </body>
 `;
 
-let counter = 0;
-
-app.get(/.*/, (req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(form);
+const respond = (res, code, content) => {
+    res.writeHead(code, {'Content-Type': 'text/html'});
+    res.write(content);
     res.send();
-});
+};
 
-app.post('/filesubmit', upload.single('file'), (req, res) => {
+const checker = (req, res) => {
+    const { body, file } = req;
+    if (file && body.name) {
+        const content = Buffer.from(file.buffer).toString("utf-8");
+        const path = `./file_${counter++}.tr`;
+        fs.writeFileSync(path, content);
+        exec(`../trenchc ${path}`, { cwd: '.'}, (err,stdout,stderr) => {
+            fs.unlinkSync(path);
+            respond(res, 200, `${form}<p>${stdout}</p>`);
+        });
+    }
+    else {   
+        respond(res, 200, `${form}<p>Please select a file AND write a name >:(</p>`);
+    }
+};
+
+const submit = (req, res) => {
     const { body, file } = req;
     if (file && body.name) {
         const content = Buffer.from(file.buffer).toString("utf-8");
@@ -70,18 +92,60 @@ app.post('/filesubmit', upload.single('file'), (req, res) => {
         fs.writeFileSync(path, content);
         exec(`../trenchc ${path}`, { cwd: '.'}, (err,stdout,stderr) => {
             if (err) fs.unlinkSync(path);
-            else console.log(`${body.name} submitted ${path}`);
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(`${form}<p>${stdout}</p>`);
-            res.send();
+            else console.log(`${body.name} submitted ${path} at ${(new Date()).toISOString()}`);
+            respond(res, 200, `${form}<p>${stdout}</p>`);
         });
     }
     else {   
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(`${form}<p>Please select a file AND write a name >:(</p>`);
-        res.send();
+        respond(res, 200, `${form}<p>Please select a file AND write a name >:(</p>`);
     }
-})
+};
+
+const game = (req, res) => {
+    const { body, file } = req;
+    if (file && body.name) {
+        const content = Buffer.from(file.buffer).toString("utf-8");
+        const path = `./file_${counter++}.tr`;
+        fs.writeFileSync(path, content);
+        exec(`../trenchc ${path}`, { cwd: '.'}, (err,stdout,stderr) => {
+            if (err) fs.unlinkSync(path);
+            else {
+                console.log(`${body.name} submitted at ${(new Date()).toISOString()}`);
+                if (body.name in map) {
+                    fs.writeFileSync(map[body.name], content);
+                    fs.unlinkSync(path);
+                } 
+                else {
+                    console.log(`${body.name} has file: ${path}`);
+                    map[body.name] = path;
+                }
+            }
+            respond(res, 200, `${form}<p>${stdout}</p>`);
+        });
+    }
+    else {   
+        respond(res, 200, `${form}<p>Please select a file AND write a name >:(</p>`);
+    }
+
+}
+
+app.get(/.*/, (req, res) => {
+    respond(res, 200, form);
+});
+
+switch (setting) {
+    case 'checker':
+        app.post('/filesubmit', upload.single('file'), checker);
+        break;
+    case 'submit':
+        app.post('/filesubmit', upload.single('file'), submit);
+        break;
+    case 'game':
+        app.post('/filesubmit', upload.single('file'), game);
+        break;
+    default:
+        throw ("Unknown setting: " + setting) 
+}
 
 const PORT = 8080;
 app.listen(PORT, () => {
